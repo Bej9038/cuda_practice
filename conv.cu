a999fb9d0ccfd8765ccbd9d0b5f77655df3133eb
Ben Jordan <bej9038@rit.edu>
2024-02-23 12:28:39 -0700

    tiled conv2D

--- conv.cu
+++ conv.cu
@@ -1,5 +1,6 @@
-#include "kernel.h"
 #include <iostream>
+#include <ctime>
+#include "kernel.h"
 
 void init_matrix(float *matrix, int width, int height, float value)
 {
@@ -31,30 +32,93 @@ void print_matrix(float *matrix, int width, int height)
     }
 }
 
-#define N 64
+#define N 28
+#define R 2
+#define BLOCK 32
+#define IN_DIM 32
+
+__constant__ float F[2*R+1][2*R+1];
+
+__global__ void conv2d_kernel_tiled_const_mem(const float *M, float *P, int r, int width, int height)
+{
+    int bx = blockIdx.x;
+    int tx = threadIdx.x;
+    int by = blockIdx.y;
+    int ty = threadIdx.y;
+
+    int col = bx * ((IN_DIM) - 2*(R)) + tx - R;
+    int row = by * ((IN_DIM) - 2*(R)) + ty - R;
+
+    __shared__ float M_s[IN_DIM][IN_DIM];
+
+    if(row >= 0 and row < height and col >= 0 and col < width)
+    { M_s[ty][tx] = M[row*width + col]; }
+    else
+    { M_s[ty][tx] = 0; }
+
+    __syncthreads();
+
+    int tileCol = tx - R;
+    int tileRow = ty - R;
+
+    if(row >= 0 and row < height and col >= 0 and col < width)
+    {
+        if(tileCol >= 0 and tileCol < ((IN_DIM) - 2*(R)) and tileRow >= 0 and tileRow < ((IN_DIM) - 2*(R)) ) {
+            float result = 0;
+            for (int i = 0; i < 2*R+1; i++) {
+                for (int j = 0; j < 2*R+1; j++) {
+                    result += F[i][j] * M_s[tileRow + i][tileCol + j];
+                }
+            }
+            P[row * width + col] = result;
+        }
+    }
+}
+
 int main()
 {
-    int k = 5;
+    int k = 2*R+1;
     int size = N*N*sizeof(float);
-    int filt_size = k*k*sizeof(float);
+    int filter_size = k * k * sizeof(float);
     float *N_h = (float*)malloc(size);
     float *P_h = (float*)malloc(size);
-    float *F_h = (float*)malloc(filt_size);
+    float *F_h = (float*)malloc(filter_size);
+
     init_matrix(N_h, N, N, 1.0);
     init_matrix(P_h, N, N, 0.0);
     init_matrix(F_h, k, k, 1.0);
+
     float *N_d, *P_d, *F_d;
     cudaMalloc(&N_d, size);
     cudaMalloc(&P_d, size);
-    cudaMalloc(&F_d, filt_size);
+    cudaMalloc(&F_d, size);
+
     cudaMemcpy(N_d, N_h, size, cudaMemcpyHostToDevice);
     cudaMemcpy(P_d, P_h, size, cudaMemcpyHostToDevice);
-    cudaMemcpy(F_d, F_h, filt_size, cudaMemcpyHostToDevice);
+    cudaMemcpy(F_d, F_h, filter_size, cudaMemcpyHostToDevice);
+    cudaMemcpyToSymbol(F, F_h, filter_size);
 
-    dim3 grid = {N/32, N/32};
-    dim3 block = {32, 32};
-    conv2d_kernel<<<grid, block>>>(N_d, F_d, P_d, 2, N, N);
+
+    dim3 grid = {1 , 1};
+    dim3 block = {BLOCK, BLOCK};
+
+    clock_t start = clock();
+//    conv2d_kernel<<<grid, block>>>(N_d, F_d, P_d, R, N, N);
+    conv2d_kernel_tiled_const_mem<<<grid, block>>>(N_d, P_d, 2, N, N);
+    cudaDeviceSynchronize();
+
+    clock_t end = clock();
+    double elapsed_seconds = double(end - start) / CLOCKS_PER_SEC;
 
     cudaMemcpy(P_h, P_d, size, cudaMemcpyDeviceToHost);
     print_matrix(P_h, N, N);
+    std::cout << "Time elapsed: " << elapsed_seconds << "s\n";
+
+//    free(N_h);
+//    free(P_h);
+//    free(F_h);
+//    cudaFree(N_d);
+//    cudaFree(P_d);
+//    cudaFree(F_d);
+
 }
